e70d10c83b7513dddbb730afa810dc302e9680bb
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-10-02 22:52:25 -0500

    when declaring seats to be type Collection we can implement seats using any concrete class that implements one of the interfaces that extend Collection. LinkedHashSet extends HashSet which implements the interface Set, which extends Collection.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-        Theater theater = new Theater("Rialto", 8, 12);
-        //just to print out all the seats
-        theater.getSeats();
+//        Theater theater = new Theater("Rialto", 8, 12);
+//        //just to print out all the seats
+//        theater.getSeats();
+//
+//        if(theater.reserveSeat("H11")) {
+//            System.out.println("Please pay");
+//        } else {
+//            System.out.println("Sorry, seat is taken.");
+//        }
+//
+//        theater.reserveSeat("A13");
+//
 
-        if(theater.reserveSeat("H11")) {
+        TheaterRevisited tr = new TheaterRevisited("Apollo", 20, 13);
+//        tr.getSeats();
+        if(tr.reserveSeat("B03")) {
             System.out.println("Please pay");
         } else {
             System.out.println("Sorry, seat is taken.");
         }
 
-        theater.reserveSeat("A13");
+        if(tr.reserveSeat("B03")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken.");
+        }
+
+        tr.reserveSeat("X10");
     }
 }
---
+++ src/TheaterRevisited.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class TheaterRevisited {
+    private final String theaterName;
+    private Collection<Seat> seats = new LinkedHashSet<>();
+
+    public TheaterRevisited(String theaterName, int numRows, int seatsPerRow) {
+        this.theaterName = theaterName;
+
+        int lastRow = 'A' + (numRows - 1);
+        for(char row = 'A'; row <= lastRow; row++) {
+            for(int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                seats.add(seat);
+            }
+        }
+    }
+
+    public String getTheaterName() {
+        return theaterName;
+    }
+
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = null;
+        for(Seat seat:seats) {
+            if(seat.getSeatNumber().equals(seatNumber)) {
+                requestedSeat = seat;
+                break;
+            }
+        }
+
+        if(requestedSeat == null) {
+            System.out.println("There is no seat " + seatNumber);
+            return false;
+        }
+
+        return requestedSeat.reserve();
+    }
+
+    //for testing
+    public void getSeats() {
+        for(Seat seat:seats) {
+            System.out.println(seat.getSeatNumber());
+        }
+    }
+
+    private class Seat {
+        private final String seatNumber;
+        private boolean reserved = false;
+
+        public Seat(String seatNumber) {
+            this.seatNumber = seatNumber;
+        }
+
+        public boolean reserve() {
+            if(!this.reserved) {
+                this.reserved = true;
+                System.out.println("Seat " + seatNumber + " reserved");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean cancel() {
+            if(this.reserved) {
+                this.reserved = false;
+                System.out.println("Reservation of seat " + seatNumber + " canceled");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public String getSeatNumber() {
+            return seatNumber;
+        }
+    }
+
+
+}
